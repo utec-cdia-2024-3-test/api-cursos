2502f46b7b3ab2731cf633e09e5ab24df5e97921
gcolchado-utec <165750492+gcolchado-utec@users.noreply.github.com>
2025-02-14 19:01:01 -0500

    Add files via upload

--- EliminarCurso.py
+++ EliminarCurso.py
@@ -2,11 +2,15 @@ import boto3
 
 def lambda_handler(event, context):
     # Entrada (json)
-    curso = event['body']
+    curso_id = event['body']['curso_id']    
     # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('cursos')
-    response = table.put_item(Item=curso)
+    response = table.delete_item(
+        Key={
+            'curso_id': curso_id
+        }
+    )
     # Salida (json)
     return {
         'statusCode': 200,
